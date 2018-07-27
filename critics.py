ec4bfc77b70b4e2610fa695113c9066ae8d1b41f
LjRokvic <ljubomirrokvic@gmail.com>
2018-07-27 15:45:49 +0200

    Changed the generator and critic convolutional layers

--- critics.py
+++ critics.py
@@ -28,7 +28,7 @@ def build_critic(img_shape):
 def build_conv_critic(img_shape):
     model = Sequential()
 
-    depth = 64
+    depth = 32
     dropout = 0.4
     # In: 1 x 28 x 28, depth=1
     # Out: 1 x 10 x 10, depth=64
@@ -36,19 +36,12 @@ def build_conv_critic(img_shape):
     in_shape = img_shape
     model.add(Conv2D(depth*1, (5, 5), strides=(2, 2), padding="same", input_shape=in_shape, data_format='channels_last'))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(dropout))
 
     model.add(Conv2D(depth*2, (5, 5), strides=(2,2), padding='same'))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(dropout))
 
     model.add(Conv2D(depth*4, (5, 5), strides=(2,2), padding='same'))
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(dropout))
-
-    model.add(Conv2D(depth*8, (5, 5), strides=(1,1), padding='same'))
-    model.add(LeakyReLU(alpha=0.2))
-    model.add(Dropout(dropout))
 
     # Out: 1-dim probability
     model.add(Flatten())
--- generator.py
+++ generator.py
@@ -35,36 +35,22 @@ def build_generator(input_size,img_shape):
 def build_conv_generator(input_size,img_shape): 
     channels = img_shape[2]
     dropout = 0.4
-    depth = 64+64+64+64
+    depth = 128
     dim = 7
 
     model = Sequential()
 
     model.add(Dense(dim*dim*depth, input_dim=input_size))
-    model.add((BatchNormalization(momentum=0.9)))
     model.add(Activation('relu'))
     model.add(Reshape((dim, dim, depth)))
-    model.add(Dropout(dropout))
-
-    # In: dim x dim x depth
-    # Out: 2*dim x 2*dim x depth/2
-    model.add(UpSampling2D(data_format='channels_last'))
-    model.add(Conv2DTranspose(int(depth/2), (5, 5), padding='same',output_shape=(None, 2*dim, 2*dim, int(depth/2)), data_format='channels_last'))
+  
+    model.add(Conv2DTranspose(int(depth/2), (5, 5),strides=(2,2), padding='same',output_shape=(None, 2*dim, 2*dim, int(depth/2)), data_format='channels_last'))
     model.add(BatchNormalization(momentum=0.9))
     model.add(Activation('relu'))    
 
-    model.add(UpSampling2D(data_format='channels_last'))
-    model.add(Conv2DTranspose(int(depth/4), (5, 5), padding='same',output_shape=(None, 4*dim, 4*dim, int(depth/4)), data_format='channels_last'))
-    model.add(BatchNormalization(momentum=0.9))
-    model.add(Activation('relu'))
-
-    model.add(Conv2DTranspose(int(depth/8), (5, 5), padding='same',output_shape=(None, 4*dim, 4*dim,int(depth/8)), data_format='channels_last'))
-    model.add(BatchNormalization(momentum=0.9))
-    model.add(Activation('relu'))
-
     # Out: 28 x 28 x 1 grayscale image [0.0,1.0] per pix
-    model.add(Conv2DTranspose(1, (5, 5), padding='same',output_shape=(None, 4*dim, 4*dim, 1), data_format='channels_last'))
-    model.add(Activation('sigmoid'))
+    model.add(Conv2DTranspose(1, (5, 5),strides=(2,2), padding='same',output_shape=(None, 4*dim, 4*dim, 1), data_format='channels_last'))
+    model.add(Activation('tanh'))
 
     noise = Input(shape=(input_size,))
     img = model(noise)
